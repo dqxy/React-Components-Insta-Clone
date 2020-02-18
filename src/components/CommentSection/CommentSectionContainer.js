a86c16815f0aefd5f84f1f04f5d5f72f7c052f6e
dqxy <yanofsky.david@gmail.com>
2020-02-18 16:53:50 -0500

    JS

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -4,12 +4,16 @@ import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
 
-const CommentSection = props => {
+const CommentSection =  ({ postId, comments }) => {
+  
   // Add state for the comments
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {comments.map(p => (
+        <Comment comment={p} />
+      ))}
       <CommentInput />
     </div>
   );
